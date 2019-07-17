c1cef876003297c680f29e7eb882a0098e1a6780
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2019-07-17 11:32:20 -0300

    codigo que procura a quantidade de pixels

---
+++ desafio.py
@@ -0,0 +1,34 @@
+###############################################
+#                                             #
+#         Desafio dos Pixels Strider          #
+# ___________________________________________ #
+#                                             #
+# Nome: Luiz Gustavo Braganca dos Santos      #
+#                                             #
+###############################################
+
+# importando a biblioteca
+from PIL import Image, ImageDraw
+
+# Metodo para ler a quantidade de pixels
+def le_quantidade():
+    # pegando a imagem
+    img = Image.open('challenge_strider.png')
+    quantidade = 0
+
+    # verifica a imagem em busca de pixels vermelhos
+    for pixel in img.getdata():
+        if pixel == (255, 0, 0):
+            quantidade += 1
+
+    return quantidade
+# end le_quantidade()
+
+
+if __name__ == "__main__":
+    # pega a quantidade
+    quantidade = le_quantidade()
+
+    # mostra a quantidade de pixels vermelhos
+    print("Quantidade =", quantidade, "pixels vermelhos")
+# end main
